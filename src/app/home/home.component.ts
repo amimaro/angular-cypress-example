4af1ad09cad23b2cd18c844c35e94155849e17df
amimaro <amir.zahlan@gmail.com>
2021-11-25 18:15:46 -0300

    Fetch Todos at Home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { combineLatest, map, Observable, startWith } from 'rxjs';
+import { FormControl } from '@angular/forms';
+
+interface Todo {
+  completed: boolean;
+  id: number;
+  title: string;
+  userId: number;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  private todos$: Observable<Todo[]>;
+  private filter$: Observable<string>;
+  public filter: FormControl;
+  public filteredTodos$: Observable<Todo[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private http: HttpClient) {
+    this.todos$ = this.http.get<Todo[]>(
+      'https://jsonplaceholder.typicode.com/todos'
+    );
+    this.filter = new FormControl('');
+    this.filter$ = this.filter.valueChanges.pipe(startWith(''));
+    this.filteredTodos$ = combineLatest([this.todos$, this.filter$]).pipe(
+      map(([todos, filterString]) =>
+        todos.filter(
+          (todo) =>
+            todo.title.toLowerCase().indexOf(filterString.toLowerCase()) !==
+              -1 && todo.userId === 1
+        )
+      )
+    );
   }
 
+  ngOnInit(): void {}
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,15 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
+  declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
-  ]
+    HomeRoutingModule,
+    HttpClientModule,
+    MatListModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
 })
-export class HomeModule { }
+export class HomeModule {}
